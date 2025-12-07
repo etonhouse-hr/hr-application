97c07109ce5f04a7f101b3ec25264ade94baaedf
etonhouse-hr <marwenelcapo4@gmail.com>
2025-12-07 11:18:40 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ MAIL_PORT = 587
 MAIL_USE_TLS = True
 
 MAIL_USERNAME = 'etonhouse'
-MAIL_PASSWORD = '7dVwS20p2UK1Di7p'
+MAIL_PASSWORD = os.getenv("SMTP_PASSWORD")
 
 MAIL_DEFAULT_SENDER = 'marwen.khalifa@etonhouse.com.sa'
 HR_EMAIL = 'marwen.khalifa@etonhouse.com.sa'
@@ -11,3 +11,4 @@ HR_EMAIL = 'marwen.khalifa@etonhouse.com.sa'
 UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = {'pdf', 'doc', 'docx'}
 SECRET_KEY = 'SecretKey123'
+
